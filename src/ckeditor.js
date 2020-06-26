04afcc4d6592f2dea9e91d2a5566f49f03a1ef76
Christopher <cspeer@gmail.com>
2020-06-26 16:03:41 +0200

    update

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -24,6 +24,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import HeadingButtonsUI from '@ckeditor/ckeditor5-heading/src/headingbuttonsui';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
 
 export default class BalloonEditor extends BalloonEditorBase {}
 
@@ -46,7 +47,8 @@ BalloonEditor.builtinPlugins = [
 	PasteFromOffice,
 	TextTransformation,
 	Heading,
-	HeadingButtonsUI
+	HeadingButtonsUI,
+	RemoveFormat
 ];
 
 // Editor configuration.
@@ -66,7 +68,8 @@ BalloonEditor.defaultConfig = {
 			'imageUpload',
 			'|',
 			'undo',
-			'redo'
+			'redo',
+			'removeFormat'
 		]
 	},
 	image: {
